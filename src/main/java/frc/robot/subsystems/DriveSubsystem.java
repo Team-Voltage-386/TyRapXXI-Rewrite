65ea2bf51330ecc3a96a844b4fc228bc22acf86b
maxhv1 <max.hero.factory@gmail.com>
2022-01-13 11:43:28 -0500

    restoreFactoryDefaults added to drive subsystem

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -12,37 +12,43 @@ import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANSparkMax.ExternalFollower;
 
 public class DriveSubsystem extends SubsystemBase {
-    // initialize motors and drivetrain
-    public final CANSparkMax m_frontLeftMotor = new CANSparkMax(Constants.DriveConstants.kFrontLeft,
-            MotorType.kBrushless);
-    public final CANSparkMax m_frontRightMotor = new CANSparkMax(Constants.DriveConstants.kFrontRight,
-            MotorType.kBrushless);
-    public final CANSparkMax m_rearLeftMotor = new CANSparkMax(Constants.DriveConstants.kRearLeft,
-            MotorType.kBrushless);
-    public final CANSparkMax m_rearRightMotor = new CANSparkMax(Constants.DriveConstants.kRearRight,
-            MotorType.kBrushless);
-    public final DifferentialDrive m_driveTrain = new DifferentialDrive(m_frontLeftMotor, m_rearRightMotor);
+        // initialize motors and drivetrain
+        public final CANSparkMax m_frontLeftMotor = new CANSparkMax(Constants.DriveConstants.kFrontLeft,
+                        MotorType.kBrushless);
+        public final CANSparkMax m_frontRightMotor = new CANSparkMax(Constants.DriveConstants.kFrontRight,
+                        MotorType.kBrushless);
+        public final CANSparkMax m_rearLeftMotor = new CANSparkMax(Constants.DriveConstants.kRearLeft,
+                        MotorType.kBrushless);
+        public final CANSparkMax m_rearRightMotor = new CANSparkMax(Constants.DriveConstants.kRearRight,
+                        MotorType.kBrushless);
+        public final DifferentialDrive m_driveTrain = new DifferentialDrive(m_frontLeftMotor, m_rearRightMotor);
 
-    public DriveSubsystem() {
-        // drivetrain
-        m_frontLeftMotor.setInverted(true);
-        m_rearRightMotor.setInverted(false);
-        m_rearLeftMotor.follow(m_frontLeftMotor);
-        m_frontRightMotor.follow(m_rearRightMotor);
-    }
+        public DriveSubsystem() {
+                // drivetrain
+                m_frontLeftMotor.restoreFactoryDefaults();
+                m_frontRightMotor.restoreFactoryDefaults();
+                m_rearLeftMotor.restoreFactoryDefaults();
+                m_rearRightMotor.restoreFactoryDefaults();
 
-    @Override
-    public void periodic() {
-        // This method will be called once per scheduler run
-        m_driveTrain.tankDrive(
-                RobotContainer.m_joystick.getRawAxis(Constants.ControllerConstants.kLeftVertical),
-                RobotContainer.m_joystick.getRawAxis(Constants.ControllerConstants.kRightVertical));
-    }
+                m_frontLeftMotor.setInverted(true);
+                m_rearRightMotor.setInverted(false);
+                m_rearLeftMotor.follow(m_frontLeftMotor);
+                m_frontRightMotor.follow(m_rearRightMotor);
+        }
 
-    // @Override
-    // public void simulationPeriodic() {
-    // // This method will be called once per scheduler run during simulation
-    // }
+        @Override
+        public void periodic() {
+                // This method will be called once per scheduler run
+                m_driveTrain.tankDrive(
+                                RobotContainer.m_joystick.getRawAxis(Constants.ControllerConstants.kLeftVertical),
+                                RobotContainer.m_joystick.getRawAxis(Constants.ControllerConstants.kRightVertical));
+        }
+
+        // @Override
+        // public void simulationPeriodic() {
+        // // This method will be called once per scheduler run during simulation
+        // }
 }
