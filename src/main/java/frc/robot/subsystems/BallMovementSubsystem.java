5474dfd15843d81e1703e2a23c02ea60ef09ab83
maxhv1 <max.hero.factory@gmail.com>
2022-01-14 20:28:01 -0500

    Intake deploy truly works now

--- src/main/java/frc/robot/subsystems/BallMovementSubsystem.java
+++ src/main/java/frc/robot/subsystems/BallMovementSubsystem.java
@@ -4,6 +4,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.BallMovementConstants.*;
 import static frc.robot.Constants.ControllerConstants.*;
 
+import org.ejml.dense.row.factory.DecompositionFactory_CDRM;
+
 import frc.robot.RobotContainer;
 
 import edu.wpi.first.wpilibj.DoubleSolenoid;
@@ -22,23 +24,23 @@ public class BallMovementSubsystem extends SubsystemBase {
 
     }
 
-    protected boolean b_intakeDeployed;
+    protected boolean d_intakeLatch = false;
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
         /* Toggle mode */
-        
-        if (RobotContainer.m_manipulatorController.getRawButtonPressed(kA)) {
-            if (m_ballPickupSolenoid.get() == DoubleSolenoid.Value.kReverse
-                    || m_ballPickupSolenoid.get() == DoubleSolenoid.Value.kOff) {
-                m_ballPickupSolenoid.set(DoubleSolenoid.Value.kForward);
-                System.out.println("deploy");
-            } else {
-                m_ballPickupSolenoid.set(DoubleSolenoid.Value.kReverse);
-                System.out.println("retract");
-            }
-        }
+        // if (RobotContainer.m_manipulatorController.getRawButtonPressed(kA)) {
+        // if (m_ballPickupSolenoid.get() == DoubleSolenoid.Value.kReverse
+        // || m_ballPickupSolenoid.get() == DoubleSolenoid.Value.kOff) {
+        // m_ballPickupSolenoid.set(DoubleSolenoid.Value.kForward);
+        // System.out.println("deploy");
+        // } else {
+        // m_ballPickupSolenoid.set(DoubleSolenoid.Value.kReverse);
+        // System.out.println("retract");
+        // }
+        // }
+
         /* A-B mode */
         // if (RobotContainer.m_manipulatorController.getRawButtonPressed(kA)
         // && (m_ballPickupSolenoid.get() == DoubleSolenoid.Value.kReverse
@@ -53,15 +55,26 @@ public class BallMovementSubsystem extends SubsystemBase {
         // m_ballPickupSolenoid.set(DoubleSolenoid.Value.kReverse);
         // System.out.println("!!!!!!retract!!!!!!");
         // }
-        /* */
 
+        /* latch switch mode */
+        if (RobotContainer.m_manipulatorController.getRawButtonPressed(kA)) {
+            d_intakeLatch = !d_intakeLatch;
+        }
+
+        if (d_intakeLatch) {
+            m_ballPickupSolenoid.set(DoubleSolenoid.Value.kForward);
+        } else {
+            m_ballPickupSolenoid.set(DoubleSolenoid.Value.kReverse);
+        }
+
+        /* baby a-b mode */
         // if(RobotContainer.m_manipulatorController.getRawButtonPressed(kA)){
         // System.out.println("A");
         // }
         // else if(RobotContainer.m_manipulatorController.getRawButtonPressed(kB)){
         // System.out.println("B");
         // }
-        System.out.println(m_ballPickupSolenoid.get());
+        // System.out.println(m_ballPickupSolenoid.get());
 
         // if
         // (RobotContainer.m_manipulatorController.getRawButton(kBallPickupForwardChannel))
