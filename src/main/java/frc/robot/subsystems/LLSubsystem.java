4a39cf69939d0ec437bfbb4d11a0e91dfbcddf8a
Carl <55468526+Carlthird@users.noreply.github.com>
2022-01-26 21:44:02 -0500

    Create LLSubsystem.java

---
+++ src/main/java/frc/robot/subsystems/LLSubsystem.java
@@ -0,0 +1,16 @@
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class LLSubsystem extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  public LLSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
