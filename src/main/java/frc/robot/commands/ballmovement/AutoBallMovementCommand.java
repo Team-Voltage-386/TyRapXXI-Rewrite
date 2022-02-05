15bc9e80b4cb83053151138ebde36950cff308f3
Daplin2 <dd1aa6560@gmail.com>
2022-02-05 13:39:05 -0500

    tweaked logic in comments, setup timer in autoball

--- src/main/java/frc/robot/commands/ballmovement/AutoBallMovementCommand.java
+++ src/main/java/frc/robot/commands/ballmovement/AutoBallMovementCommand.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands.ballmovement;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import frc.robot.RobotContainer;
@@ -11,9 +12,10 @@ import frc.robot.subsystems.BallMovementSubsystem;
 
 import static frc.robot.Constants.ControllerConstants.*;
 
-public class AutoBallMovementCommand  extends CommandBase {
-    BallMovementSubsystem subsystem;
-
+public class AutoBallMovementCommand extends CommandBase {
+  BallMovementSubsystem subsystem;
+  Timer overallMotorTimer = new Timer();
+  Timer motorTimer = new Timer();
   protected boolean intakeLatch = false;// intake deployed status
 
   /** Creates a new ManualBallMovementCommand. */
@@ -26,8 +28,8 @@ public class AutoBallMovementCommand  extends CommandBase {
   // Called when the command is initially scheduled
   @Override
   public void initialize() {
-      // Set initial state of intake as retracted
-      subsystem.retractIntake();
+    // Set initial state of intake as retracted
+    subsystem.retractIntake();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -50,39 +52,49 @@ public class AutoBallMovementCommand  extends CommandBase {
     subsystem.runLauncher(RobotContainer.manipulatorController.getRawAxis(kLeftTrigger));
     subsystem.runSerializerMotor(RobotContainer.manipulatorController.getRawAxis(kLeftVertical));
     subsystem.runIntakeMotor(RobotContainer.manipulatorController.getRawAxis(kRightTrigger));
-
-    if (RobotContainer.manipulatorController.getRawButtonPressed(kB)) {
-        //Run feeder motor and serializer motor for set time to shoot 3 balls
-    }
-    else {
-        if (subsystem.getEntranceSensor() == true && subsystem.getIndexerSensor() == false && subsystem.getFeederSensor() == false) {
-            //Make serializer run
+    boolean launch = false;
+    if (RobotContainer.manipulatorController.getRawButtonPressed(kB) == true) {
+      launch = true;
+      overallMotorTimer.start();
+      if (overallMotorTimer.get() <= 5) { // dont actually know what the set time set is, assuming 5 seconds for ability to test code 
+        // run feeder motor and serializer motor for set time to shoot 3 balls
+      } else {
+        // stop running feeder and serializer
+      }
+    } else if (launch == true) {
+      if (overallMotorTimer.get() <= 5) {
+        // continue running feeder and serializer motor for the set amount of time
+      }
+    } else {
+      if (subsystem.getIndexerSensor() == false && subsystem.getFeederSensor() == true) {
+        // make serializer and feeder (motor) run
+      } else if (subsystem.getEntranceSensor() == false && subsystem.getIndexerSensor() == false
+          && subsystem.getFeederSensor() == false) {
+        // do nothing (new)
+      } else if (subsystem.getEntranceSensor() == true && subsystem.getIndexerSensor() == true
+          && subsystem.getFeederSensor() == true) {
+        motorTimer.start();
+        if (motorTimer.get() <= 1) {
+          // run serializer for short period of time to have all balls in serializer
+          // dont actually know how long  it should take, assuming 1 second   for ability to test code
         }
-        else if (subsystem.getEntranceSensor() == false && subsystem.getIndexerSensor() == true && subsystem.getFeederSensor() == false) {
-            //Make serializer false
-        }
-        else if (subsystem.getEntranceSensor() == true && subsystem.getIndexerSensor() == true && subsystem.getFeederSensor() == false) {
-            //Make serializer run
-        }
-        else if (subsystem.getEntranceSensor() == false && subsystem.getIndexerSensor() == true && subsystem.getFeederSensor() == true) {
-            //Make serializer false
-        }
-        else if (subsystem.getEntranceSensor() == true && subsystem.getIndexerSensor() == true && subsystem.getFeederSensor() == true) {
-            //Run serializer for short period of time to have all balls in serializer
-        }
+      } else if (subsystem.getEntranceSensor() == true) {
+        // make serializer run
+      } else { // entrance senor is false
+        // make serializer false
+      }
     }
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return false;
-  }   
+  }
 
 }
-
-
